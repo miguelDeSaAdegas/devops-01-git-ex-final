c70f89b84d39740eaf7463c1edaeb7919ade0a52
cdhaeyere <114183609+cdhaeyere@users.noreply.github.com>
2022-10-04 17:42:27 +0200

    fix : initials of collaborator 1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 const mounth = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]
 
 	res.render('./views/Home', {
-		title: 'MA',
+		title: 'MA - CD',
 		cards: [
 			{
 				title: 'Our Changing Planet',
